8f84d6dcb7e76b217a8c96f9c866be1ab415a56a
Marco Schumann <marco.schumann@pyur.com>
2026-01-29 10:18:21 +0100

    fix: Progress to build sharable worker

--- dist/index.cjs
+++ dist/index.cjs
@@ -1,3 +1,30 @@
+"use strict";
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+// src/index.ts
+var index_exports = {};
+__export(index_exports, {
+  BrowserCounter: () => BrowserCounter,
+  NodeCounter: () => NodeCounter
+});
+module.exports = __toCommonJS(index_exports);
+
 // src/adapter/browserCounter.ts
 var BrowserCounter = class {
   constructor(worker) {
@@ -55,7 +82,8 @@ var NodeCounter = class {
 function isMockWorker(worker) {
   return worker && "on" in worker && "message" in worker.on;
 }
-export {
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {
   BrowserCounter,
   NodeCounter
-};
+});
--- dist/worker/counter.worker.cjs
+++ dist/worker/counter.worker.cjs
@@ -1,3 +1,5 @@
+"use strict";
+
 // src/core/counter.ts
 var CounterCore = class {
   constructor() {
